15e86196692071041f392bb6ca5e366df1323e03
Erick-Rangel <rebel863@hotmail.com>
2022-07-04 17:48:42 -0500

    first commit

---
+++ src/Punto2.java
@@ -0,0 +1,11 @@
+public class Punto2 {
+    static void punto2(String[] args) {
+        int numeroWhile = 0;
+        while (numeroWhile < 3) {
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        }
+    }
+}
+
+
